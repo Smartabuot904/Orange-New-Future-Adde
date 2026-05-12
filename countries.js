620ec8cff15e37d2c6377068aea983d0282221dc
Smartabuot904 <smartquotex9045@gmail.com>
2026-05-12 15:40:09 +0600

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,85 @@
+export const countries = [
+  {
+    name: "Italy",
+    code: "+39",
+    flag: "🇮🇹",
+    lang: ""
+  },
+
+  {
+    name: "United Kingdom",
+    code: "+44",
+    flag: "🇬🇧",
+    lang: ""
+  },
+
+  {
+    name: "Japan",
+    code: "+81",
+    flag: "🇯🇵",
+    lang: ""
+  },
+
+  {
+    name: "Pakistan",
+    code: "+92",
+    flag: "🇵🇰",
+    lang: ""
+  },
+
+  {
+    name: "Oman",
+    code: "+968",
+    flag: "🇴🇲",
+    lang: ""
+  },
+
+  {
+    name: "China",
+    code: "+86",
+    flag: "🇨🇳",
+    lang: ""
+  },
+
+  {
+    name: "Qatar",
+    code: "+974",
+    flag: "🇶🇦",
+    lang: ""
+  },
+
+  {
+    name: "Bahrain",
+    code: "+973",
+    flag: "🇧🇭",
+    lang: ""
+  },
+
+  {
+    name: "Norway",
+    code: "+47",
+    flag: "🇳🇴",
+    lang: ""
+  },
+
+  {
+    name: "Indonesia",
+    code: "+62",
+    flag: "🇮🇩",
+    lang: ""
+  },
+
+  {
+    name: "Singapore",
+    code: "+65",
+    flag: "🇸🇬",
+    lang: ""
+  },
+
+  {
+    name: "Iran",
+    code: "+98",
+    flag: "🇮🇷",
+    lang: "ja"
+  }
+];
