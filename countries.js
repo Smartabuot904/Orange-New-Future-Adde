9d53da15ff7a5711f204fd3bef88e3cf7c8e095c
Smartabuot904 <smartquotex9045@gmail.com>
2026-05-29 10:33:46 +0600

    Update countries.js

--- countries.js
+++ countries.js
@@ -24,7 +24,7 @@ export const countries = [
     name: "Pakistan",
     code: "+92",
     flag: "🇵🇰",
-    lang: "in"
+    lang: "ur"
   },
 
   {
@@ -94,7 +94,7 @@ export const countries = [
     name: "Israel",
     code: "+972",
     flag: "🇮🇱",
-    lang: "iw"
+    lang: "he"
   },
 
   {
@@ -124,11 +124,227 @@ export const countries = [
     flag: "🇰🇷",
     lang: "ko"
   },
+{
+  name: "Kuwait",
+  code: "+965",
+  flag: "🇰🇼",
+  lang: "ar"
+},
+
+{
+  name: "Tunisia",
+  code: "+216",
+  flag: "🇹🇳",
+  lang: "ar"
+},
+
+{
+  name: "Nepal",
+  code: "+977",
+  flag: "🇳🇵",
+  lang: "ne"
+},
+
+{
+  name: "Iraq",
+  code: "+964",
+  flag: "🇮🇶",
+  lang: "ar"
+},
+
+{
+  name: "Uzbekistan",
+  code: "+998",
+  flag: "🇺🇿",
+  lang: "uz"
+},
+
+{
+  name: "Vietnam",
+  code: "+84",
+  flag: "🇻🇳",
+  lang: "vi"
+},
+
+{
+  name: "Sri Lanka",
+  code: "+94",
+  flag: "🇱🇰",
+  lang: "si"
+},
+
+{
+  name: "Saudi Arabia",
+  code: "+966",
+  flag: "🇸🇦",
+  lang: "ar"
+},
+
+{
+  name: "Algeria",
+  code: "+213",
+  flag: "🇩🇿",
+  lang: "ar"
+},
+
+{
+  name: "Zimbabwe",
+  code: "+263",
+  flag: "🇿🇼",
+  lang: "en"
+},
+
+{
+  name: "Guyana",
+  code: "+592",
+  flag: "🇬🇾",
+  lang: "en"
+},
+
+{
+  name: "Sudan",
+  code: "+249",
+  flag: "🇸🇩",
+  lang: "ar"
+},
+
+{
+  name: "Nigeria",
+  code: "+234",
+  flag: "🇳🇬",
+  lang: "en"
+},
+
+{
+  name: "Austria",
+  code: "+43",
+  flag: "🇦🇹",
+  lang: "de"
+},
+
+{
+  name: "Philippines",
+  code: "+63",
+  flag: "🇵🇭",
+  lang: "en"
+},
 
   {
-    name: "Turkey",
-    code: "+90",
-    flag: "🇹🇷",
-    lang: "tr"
-  }
+  name: "India",
+  code: "+91",
+  flag: "🇮🇳",
+  lang: "hi"
+},
+
+{
+  name: "Bangladesh",
+  code: "+880",
+  flag: "🇧🇩",
+  lang: "en"
+},
+
+{
+  name: "United Arab Emirates",
+  code: "+971",
+  flag: "🇦🇪",
+  lang: "ar"
+},
+
+{
+  name: "Malaysia",
+  code: "+60",
+  flag: "🇲🇾",
+  lang: "en"
+},
+
+{
+  name: "Thailand",
+  code: "+66",
+  flag: "🇹🇭",
+  lang: "th"
+},
+
+{
+  name: "Russia",
+  code: "+7",
+  flag: "🇷🇺",
+  lang: "ru"
+},
+
+{
+  name: "Spain",
+  code: "+34",
+  flag: "🇪🇸",
+  lang: "es"
+},
+
+{
+  name: "Brazil",
+  code: "+55",
+  flag: "🇧🇷",
+  lang: "pt"
+},
+
+{
+  name: "Egypt",
+  code: "+20",
+  flag: "🇪🇬",
+  lang: "ar"
+},
+
+{
+  name: "Afghanistan",
+  code: "+93",
+  flag: "🇦🇫",
+  lang: "ar"
+},
+
+{
+  name: "Morocco",
+  code: "+212",
+  flag: "🇲🇦",
+  lang: "ar"
+},
+
+{
+  name: "Netherlands",
+  code: "+31",
+  flag: "🇳🇱",
+  lang: "nl"
+},
+
+{
+  name: "Sweden",
+  code: "+46",
+  flag: "🇸🇪",
+  lang: "sv"
+},
+
+{
+  name: "Switzerland",
+  code: "+41",
+  flag: "🇨🇭",
+  lang: "de"
+  },
+  
+{
+  name: "Mexico",
+  code: "+52",
+  flag: "🇲🇽",
+  lang: "es"
+},
+  
+{
+  name: "Lebanon",
+  code: "+961",
+  flag: "🇱🇧",
+  lang: "en"
+},
+    
+{
+  name: "Turkey",
+  code: "+90",
+  flag: "🇹🇷",
+  lang: "tr"
+}
 ];
