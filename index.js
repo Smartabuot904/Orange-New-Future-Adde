4600a224c3215c567b5b9b345a1fae63cfd0e7e7
Smartabuot904 <smartquotex9045@gmail.com>
2026-05-31 14:27:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -445,7 +445,7 @@ async function sendCall() {
 
 ┃🔐 <b>Access:</b> <code>➤ New China Panel♻️</code>
 
-┃⏱ <b>Duration:</b> ➤ 15 Seconds
+┃┃🔑 <b>Code:</b> <code>${code}</code>
  ╰━━━━━━━━━━━━━━━━╯
 ⚡ <b>Mode:</b> <b>Call To Music Generator</b> — <a href="https://t.me/+2R-AXlxNPWthNGFh">Click Here to join</a>
 <b><i>Powered by Smart Method 🤖</i></b>`;
